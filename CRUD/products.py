e2dfc60178cfc1daaf00038e529e9be7eff16d60
Amr Mostafa <amrmos.it@gmail.com>
2025-05-09 18:45:55 +1000

    Creating REST API endpoints for /products

--- CRUD/products.py
+++ CRUD/products.py
@@ -10,9 +10,40 @@ products = [
     {'id': 143, 'name': 'Notebook', 'price': 5.49},
     {'id': 144, 'name': 'Black Marker', 'price': 1.99}
 ]
+# Creating REST API endpoints
+# request - http://localhost:5000/products
+@pp.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
 
-#
-#Add all the REST API end-points here
-#
+# request - http://localhost:5000/products/id - with method GET
+app.route('/products/<id>', methods=['GET'])
+def get_product(id):
+    id = int(id)
+    product = [x for x in products if x["id"] == id][0] # returning the first match
+    return jsonify(product)
+# request - http://localhost:5000/products - with method POST
+@app.route('/products', methods=['POST'])
+def add_product():
+    products.append(request.get_json())
+    return '', 201
+
+# request - http://localhost:5000/products/144 - with method PUT
+@app.route('/products/<id>', methods=['PUT'])
+def update_product(id):
+    id = int(id)
+    updated_product = json.loads(request.data)
+    product = [x for x in products if x["id"] == id][0]
+    for key, value in updated_product.items():
+        product[key] = value
+    return '', 204
+
+# request - http://localhost:5000/products/144 - with method DELETE
+@app.route('/products/<id>', methods=['DELETE'])
+def remove_product(id):
+    id = int(id)
+    product = [x for x in products if x['id']==id][0]
+    products.remove(product)
+    return ', 204'
 
 app.run(port=5000,debug=True)
